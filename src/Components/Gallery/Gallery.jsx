657c7d79d80fc989d1f6157901d7adf5567dbb3b
javid <graphicjr409@gmail.com>
2026-03-15 02:18:23 +0500

    Fix case-sensitive CSS import for Gallery component

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { imagesGallry } from './Images'
-import './Galleryimg.css'
+import React from "react";
+import { imagesGallry } from "./Images";
+import "./galleryimg.css";
 const Gallery = () => {
   return (
-    <div className='gallery-container'>
-     <h2>@UOMO</h2>   
-     <div className="gallery-images">
-      {imagesGallry.map((img , ind)=>(
-      <img key={ind} src={img}  alt={`gallery-${ind}`}/>  
-      ))}  
-     </div>
+    <div className="gallery-container">
+      <h2>@UOMO</h2>
+      <div className="gallery-images">
+        {imagesGallry.map((img, ind) => (
+          <img key={ind} src={img} alt={`gallery-${ind}`} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
